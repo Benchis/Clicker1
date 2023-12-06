80114ee1b5b82f748ffb0a0f8c35f6f5cdf5937c
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-12-06 16:45:36 +0200

    Gamemanager and Text

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,18 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int clicks;
+    public TMP_Text clicksText;
+
+    int _clicks;
+    public int Clicks
+    {
+        get { return _clicks; }
+        set
+        {
+            _clicks = value;
+            clicksText.text = _clicks.ToString();
+        }
+    }
 }
